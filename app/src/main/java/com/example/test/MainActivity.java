c3e3c0d71a7d66686206ce8b0fde06c373d20348
hyezzi-ya <hyezzi-ya@naver.com>
2022-04-07 13:24:33 +0900

    list 수정중

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -1,10 +1,14 @@
+///////// 수
+
 package com.example.test;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,10 +26,9 @@ import com.naver.maps.map.overlay.Marker;
 import com.naver.maps.map.overlay.Overlay;
 import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
-import com.naver.maps.map.widget.CompassView;
 import com.naver.maps.map.widget.LocationButtonView;
-import com.naver.maps.map.widget.ScaleBarView;
-import com.naver.maps.map.widget.ZoomControlView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,Overlay.OnClickListener  {
 
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     };
 
     private FusedLocationSource mLocationSource;
-    private NaverMap mNaverMap;
+    private NaverMap naverMap;
 
     // 마커를 찍을 데이터
     //private ArrayList<PlaceInfo> mPlaceInfoList;
@@ -63,47 +66,55 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // 위치를 반환하는 구현체인 FusedLocationSource 생성
         mLocationSource =
                 new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+        // Load Database (Rest)
+        List<Rest> restList = initLoadRestDatabase();
+
+        // Add Rest Marker
+        addRestMarker(restList);
+
+
+
     }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
         Log.d( TAG, "onMapReady");
 
-        // 지도상에 마커 표시
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(37.4908, 126.919));
-        marker.setMap(naverMap);
-
-        marker.setWidth(100);
-        marker.setHeight(100);
-        // 마커 아이콘변경
-//        marker.setIcon(OverlayImage.fromResource(R.drawable.tour));
-        marker.setOnClickListener(this);
-
-        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
-        mNaverMap = naverMap;
-        mNaverMap.setLocationSource(mLocationSource);
-
-        // UI 컨트롤 재배치
-        UiSettings uiSettings = mNaverMap.getUiSettings();
-//        uiSettings.setCompassEnabled(false); // 기본값 : true
-//        uiSettings.setScaleBarEnabled(false); // 기본값 : true
-//        uiSettings.setZoomControlEnabled(false); // 기본값 : true
-        uiSettings.setLocationButtonEnabled(false); // 기본값 : false
-//        uiSettings.setLogoGravity(Gravity.RIGHT|Gravity.BOTTOM);
-
-//        CompassView compassView = findViewById(R.id.compass);
-//        compassView.setMap(mNaverMap);
-//        ScaleBarView scaleBarView = findViewById(R.id.scalebar);
-//        scaleBarView.setMap(mNaverMap);
-//        ZoomControlView zoomControlView = findViewById(R.id.zoom);
-//        zoomControlView.setMap(mNaverMap);
-        LocationButtonView locationButtonView = findViewById(R.id.location);
-        locationButtonView.setMap(mNaverMap);
-
-
+        this.naverMap = naverMap;
         // 권한확인. 결과는 onRequestPermissionsResult 콜백 매서드 호출
         ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQUEST_CODE);
+
+
+
+
+
+
+
+//        // 지도상에 마커 표시
+//        Marker marker = new Marker();
+//        marker.setPosition(new LatLng(37.4908, 126.919));
+//        marker.setMap(naverMap);
+//
+//        marker.setWidth(100);
+//        marker.setHeight(100);
+        // 마커 아이콘변경
+//        marker.setIcon(OverlayImage.fromResource(R.drawable.tour));
+//        marker.setOnClickListener(this);
+
+        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        this.naverMap = naverMap;
+        this.naverMap.setLocationSource(mLocationSource);
+
+
+
+        // UI 컨트롤 재배치
+        UiSettings uiSettings = this.naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(false); // 기본값 : false
+
+        LocationButtonView locationButtonView = findViewById(R.id.location);
+        locationButtonView.setMap(this.naverMap);
+
+
     }
 
     @Override
@@ -114,7 +125,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
             }
         }
     }
@@ -129,4 +140,46 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
 
+    public void addRestMarker(@NonNull List<Rest> restList) {
+
+        // Marker img -> bitmap
+        //Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.markerline_yellow);
+
+        for (int i = 0; i < restList.size(); i++) {
+
+            String storeName = restList.get(i).storeName;     // 이름
+            // 주소
+            double lat = restList.get(i).latitude;            // 위도
+            double lon = restList.get(i).longitude;           // 경도
+
+
+            Marker marker = new Marker();
+            marker.setPosition(new LatLng(lat, lon));
+            marker.setMap(naverMap);
+//            marker.setCaptionText(storeName);
+            marker.setAnchor(new PointF(0.5f, 1.0f));
+            marker.setOnClickListener(this);
+            //markerList.add(marker);
+
+
+
+        }
+    }
+    public List<Rest> initLoadRestDatabase(){
+        DatabaseHelper databaseHelper = new DatabaseHelper(getApplicationContext());
+        databaseHelper.OpenDatabaseFile();
+
+        List<Rest> restList = databaseHelper.getTableData();
+        Log.e("test", String.valueOf(restList.size()));
+
+        databaseHelper.close();
+        return restList;
+    }
+
+
+
+
+
+
 }
+
---
+++ app/src/main/java/com/example/test/Rest.java
@@ -0,0 +1,39 @@
+package com.example.test;
+
+
+// 데이터베이스 테이블을 저장할 Class 작성
+public class Rest {
+
+    public int _id;
+    public String storeName;
+    public String address;
+    public double longitude;
+    public double latitude;
+    public String time;
+    public String h_day;
+    public String date_fd;
+
+
+
+    // set function
+    public void setId(int _id){ this._id = _id; }
+    public void setStoreName(String storeName){ this.storeName = storeName; }
+    public void setAddress(String address){ this.address = address; }
+    public void setLongitude(double longitude){ this.longitude = longitude;}
+    public void setLatitude(double latitude){this.latitude = latitude;}
+    public void setTime(String time){ this.time = time; }
+    public void setH_day(String h_day){ this.h_day = h_day; }
+    public void setDate_fd(String date_fd){ this.date_fd = date_fd; }
+
+    // get function
+    public int getId(){ return this._id; }
+    public String getStoreName(){ return this.storeName; }
+    public String getAddress(){ return this.address; }
+    public double getLatitude(){ return this.latitude;}
+    public double getLongitude(){return this.longitude;}
+    public String getTime(){ return this.time; }
+    public String getH_day(){ return this.h_day; }
+    public String getDate_fd(){ return this.date_fd; }
+
+
+}
